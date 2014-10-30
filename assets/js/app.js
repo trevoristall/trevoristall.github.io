c7892c0b19267e53c4122505fd33e02c2945938b
Trevor Allen <trevor@ampleheights.com>
2014-10-30 09:44:56 -0400

    js optimization

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,9 +13,9 @@ $(document).ready(function ($) {
     }
 
     $('body').velocity('transition.fadeIn', {duration: 450});
-    $('.contact-form').find('input, textarea, label').velocity('transition.slideLeftBigIn', {delay: 500, stagger: 150, drag: true});
-    $('.masthead').find('h1').velocity('transition.slideUpBigIn', {delay: 500, duration: 500});
-    $('.grid').find("figure").velocity('transition.slideLeftBigIn', {delay: 500, stagger: 150, drag: true});
-    $('.profile-image').find('img').velocity('transition.slideDownBigIn', {delay: 500, duration: 600});
+    $('.contact-form').find('input, textarea, label').velocity('transition.slideLeftBigIn', {stagger: 125, drag: true});
+    $('.masthead').find('h1').velocity('transition.slideUpBigIn', {duration: 500});
+    $('.grid').find("figure").velocity('transition.slideLeftBigIn', {stagger: 150, drag: true});
+    $('.profile-image').find('img').velocity('transition.slideDownBigIn', {duration: 600});
 
 });
