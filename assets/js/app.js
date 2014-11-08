51a1fc9a45c65633d6ae9f3b4c2bc44fb1c66eae
Trevor Allen <trevor@ampleheights.com>
2014-11-07 21:46:49 -0500

    removing a lot

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,13 @@
 
     'use strict';
     var doc = document,
-        body = doc.querySelectorAll("body"),
-        input = doc.querySelectorAll("input"),
-        textarea = doc.querySelectorAll("textarea"),
-        label = doc.querySelectorAll("label"),
-        figure = doc.querySelectorAll("figure");
+        // body = doc.querySelectorAll("body"),
+        // input = doc.querySelectorAll("input"),
+        // textarea = doc.querySelectorAll("textarea"),
+        // label = doc.querySelectorAll("label"),
+        figure = doc.querySelectorAll("figure"),
+        header = doc.querySelectorAll(".masthead h1");
+
 
 
     if (!Modernizr.svg) {
@@ -16,7 +18,8 @@
     }
 
     //$('.contact-form').find('input, textarea, label').velocity('transition.slideLeftBigIn', {delay: 400, stagger: 125, drag: true});
-    // $('.masthead').find('h1').velocity('transition.slideUpBigIn', {delay: 400, duration: 500});
+    // $('.masthead').find('h1').
+    Velocity(header, 'transition.slideUpBigIn', {delay: 400, duration: 500});
     // $('.grid').find("figure")
     Velocity(figure, 'transition.slideLeftBigIn', {delay: 400, stagger: 150, drag: true});
     // $('.profile-image').find('img').velocity('transition.slideDownBigIn', {delay: 400, duration: 600});
