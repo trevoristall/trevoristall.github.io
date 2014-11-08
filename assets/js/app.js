b0de9d9d5d975fd0bdb5a8d31a35c320be10e204
Trevor Allen <trevor@ampleheights.com>
2014-11-07 21:27:48 -0500

    removing Zepto

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
     'use strict';
     var doc = document,
-        body = doc.querySelectorAll("body");;
+        body = doc.body;
 
 
     if (!Modernizr.svg) {
@@ -11,10 +11,10 @@
         });
     }
 
-    body.velocity( 'transition.slideDownBigIn', {duration: 400});
-    $('.contact-form').find('input, textarea, label').velocity('transition.slideLeftBigIn', {delay: 400, stagger: 125, drag: true});
-    $('.masthead').find('h1').velocity('transition.slideUpBigIn', {delay: 400, duration: 500});
-    $('.grid').find("figure").velocity('transition.slideLeftBigIn', {delay: 400, stagger: 150, drag: true});
-    $('.profile-image').find('img').velocity('transition.slideDownBigIn', {delay: 400, duration: 600});
+    body.velocity('transition.slideDownBigIn', {duration: 400});
+    // $('.contact-form').find('input, textarea, label').velocity('transition.slideLeftBigIn', {delay: 400, stagger: 125, drag: true});
+    // $('.masthead').find('h1').velocity('transition.slideUpBigIn', {delay: 400, duration: 500});
+    // $('.grid').find("figure").velocity('transition.slideLeftBigIn', {delay: 400, stagger: 150, drag: true});
+    // $('.profile-image').find('img').velocity('transition.slideDownBigIn', {delay: 400, duration: 600});
 
 }());
