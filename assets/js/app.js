0f882372f82053a4b0bf15d57a42bdbf67b51d01
Trevor Allen <trevor@ampleheights.com>
2014-11-28 18:14:33 -0500

    some animation tweaks and updated URLs

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,22 +12,21 @@
         header = q('.masthead h1'),
         links = q('.main-nav a');
 
+        // allow command-click and control-click to open new tab
         links.onclick = function() {
           if (event.metaKey || event.ctrlKey) {
             return;
           } else {
-            event.preventDefault(function(){
-              Velocity(body, 'transition.slideUpBigOut', {duration: 400});
-            });
+            event.preventDefault( Velocity(body, 'transition.fadeOut', {duration: 400}) );
           }
         };
-        // allow command-click and control-click to open new tab
 
 
-    Velocity(body, 'transition.slideUpBigIn', {duration: 400});
-    Velocity(form, 'transition.slideLeftBigIn', { delay: 400, stagger: 125, drag: true});
-    Velocity(header, 'transition.slideUpBigIn', { delay: 400, duration: 1000});
-    Velocity(figure, 'transition.slideLeftBigIn', { delay: 400, stagger: 150, drag: true});
-    Velocity(profile, 'transition.slideDownBigIn', { delay: 400, duration: 1000});
+
+    Velocity(body, 'transition.fadeIn', {duration: 400});
+    Velocity(form, 'transition.slideLeftBigIn', { delay: 350, stagger: 125, drag: true});
+    Velocity(header, 'transition.slideUpBigIn', { delay: 350, duration: 1000});
+    Velocity(figure, 'transition.slideLeftBigIn', { delay: 350, stagger: 150, drag: true});
+    Velocity(profile, 'transition.slideDownBigIn', { delay: 350, duration: 1000});
 
 }());
